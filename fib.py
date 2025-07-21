458fa031dd291e206c949f730f15202b730d63f8
Saurabha R Shetty <166347555+saurabhaRshetty@users.noreply.github.com>
2025-07-21 23:24:27 +0530

    Update fib.py

---
+++ fib.py
@@ -0,0 +1,20 @@
+def fibo(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+a = int(input("Enter any number "))
+for i in range(a):
+    print(fibo(i), end=" ")  # Output the Fibonacci series up to n
+    
+
+
+
+
+
+    
+
